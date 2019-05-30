d273ce43191cf715bf4b2046dbf32a6696d82103
bianhy <bianhaiyang@songshuai.com>
2019-05-30 16:51:12 +0800

    swoole

---
+++ swoole/tcp_server.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bianhy
+ * Date: 2019/5/30
+ * Time: 16:36
+ */
+
+$server = new swoole_server("127.0.0.1", 9503);
+$server->on('connect', function ($server, $fd){
+    echo "connection open: {$fd}\n";
+});
+$server->on('receive', function ($server, $fd, $reactor_id, $data) {
+    $server->send($fd, "Swoole: {$data}");
+    $server->close($fd);
+});
+$server->on('close', function ($server, $fd) {
+    echo "connection close: {$fd}\n";
+});
+$server->start();
